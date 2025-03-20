f9c521eae46e27479a8add9a65392259bc7fe001
cristianJLG <cristianlozano00001@gmail.com>
2025-03-19 19:13:25 -0600

    Fead:Added Components

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,28 @@ import Tarjeta from "./components/Tarjeta";
 import TablaCuentas from "./components/TablaCuentas";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Alert from "./components/Alert";
+import Button from "./components/Button"
+import Dropdown from "./components/Dropdown";
+import Upload from "./components/Upload";
+import Choices from "./components/Choices";
+import ReadyAlert from "./components/ReadyAlert";
 
 function App() {
   return (
     <div>
-
       <BotonMenu />
       <Buscador />
       <Tarjeta />
       <TablaCuentas />
       <Header/>
       <Nav/>
+      <Alert/>
+      <Upload/>
+      <Button/>
+      <Dropdown/>
+      <Choices/>
+      <ReadyAlert/>
     </div>
   );
 }
