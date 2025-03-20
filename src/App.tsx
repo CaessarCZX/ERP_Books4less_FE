ca91f86e3851253b27f11396a2bc47ce2764f35a
Miguel <migueltony9@gmail.com>
2025-03-19 19:13:56 -0600

    Feat: Added Components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Tarjeta from "./components/Tarjeta";
 import TablaCuentas from "./components/TablaCuentas";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Card from "./components/Card";
+
 
 function App() {
   return (
@@ -13,8 +15,10 @@ function App() {
       <Buscador />
       <Tarjeta />
       <TablaCuentas />
-      <Header/>
+      <Header />
       <Nav/>
+      <Card />
+      
     </div>
   );
 }
