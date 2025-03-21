f6bfae8af2563b69cbfabfc158daf7677699b54f
Miguel <migueltony9@gmail.com>
2025-03-21 11:06:09 -0600

    Feat:Screen preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
-// import BotonMenu from "./components/BotonMenu";
-// import Buscador from "./components/Buscador";
-// import Tarjeta from "./components/Tarjeta";
-// import TablaCuentas from "./components/TablaCuentas";
-// import Header from "./components/Header";
-// import Nav from "./components/Nav";
-// import Card from "./components/Card";
-
-
-// import Alert from "./components/Alert";
-// import Button from "./components/Button"
-// import Dropdown from "./components/Dropdown";
-// import Upload from "./components/Upload";
-// import Choices from "./components/Choices";
-// import ReadyAlert from "./components/ReadyAlert";
-import Form from "./components/Form";
+import './styles/App.css'
+import Header from "./components/Header.tsx";
+import AsideNav from './components/AsideNav.tsx';
+import DropZone from './components/DropZone.tsx';
+import Formulario from './components/Formulario.tsx';
 
 function App() {
   return (
-    <div>
-      {/* <BotonMenu />
-      <Buscador />
-      <Tarjeta />
-      <TablaCuentas />
-      <Header />
-      <Nav/>
-      <Card />
-      <Alert/>
-      <Upload/>
-      <Button/>
-      <Dropdown/>
-      <Choices/>
-      <ReadyAlert/> */}
-      <Form/>
+    <div className='principalContainer'>   
+      <aside>
+        <AsideNav />  
+      </aside>
+      <main>
+        <Header />
+        <DropZone />
+        <Formulario />
+      </main>
+     
     </div>
   );
 }
