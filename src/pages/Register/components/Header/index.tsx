b26783f00e7796ff844b030bf69bfee36401a2f1
Miguel <migueltony9@gmail.com>
2025-05-08 11:52:47 -0600

    Fix: btn arrow register

--- src/pages/Register/components/Header/index.tsx
+++ src/pages/Register/components/Header/index.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
           className="py-2.375 mr-4 ml-4 text-sm font-bold whitespace-nowrap text-white lg:ml-0"
           to={getPrivateRoute('GENERATE_PO')}
         >
-          <FaLongArrowAltLeft size={35} className="ml-5" />
+          <FaLongArrowAltLeft className="ml-10 h-16 w-12 -mt-1" />
         </Link>
       </div>
     </nav>
