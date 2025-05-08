b2f3374f724c7792615b100c4a2ab1ef5efa4bfc
cristianJLG <cristianlozano00001@gmail.com>
2025-05-08 08:34:30 -0600

    CHORE: IU, Notificaciones ISBN

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -11,7 +11,7 @@ const DefaultLayout: FC = () => {
       <div className="relative flex flex-1 flex-col lg:ml-72.5">
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <main>
-          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-0">
             <Outlet />
           </div>
         </main>
